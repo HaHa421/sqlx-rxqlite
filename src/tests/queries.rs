180166276911d285b3b739427d8d60becc6f3c73
Your Name <you@example.com>
2024-04-08 08:46:42 +0200

    add many simultaneous connections test

--- src/tests/queries.rs
+++ src/tests/queries.rs
@@ -114,3 +114,65 @@ fn many_connections() {
         join_all(futs).await;
     });
 }
+
+#[test]
+fn many_connections_with_options() {
+    let rt = Runtime::new().unwrap();
+    let _ = rt.block_on(async {
+        //const QUERY: &str ="SELECT name,birth_date from _test_user_ where name = ?";
+        let /*mut*/ tm = TestManager::new("many_connections_with_options",3,None);
+        tm.wait_for_cluster_established(1, 60).await.unwrap();
+        const CONNECTIONS_COUNT:usize=100;
+        let mut futs = vec![];
+        let mut rng = rand::thread_rng();
+        for i in 0..CONNECTIONS_COUNT {
+          let node_id = rng.gen_range(1..tm.instances.len()+1);
+          let http_addr = tm.instances.get(&(node_id as u64)).unwrap().http_addr.clone();
+          futs.push(async move{
+            let mut connect_options = RXQLiteConnectOptions::default();
+            connect_options = connect_options.node_id(node_id as _);
+            let mut split= http_addr.split(":");
+            
+            connect_options = connect_options.host(split.next().unwrap());
+            connect_options = connect_options.port(split.next().unwrap().parse::<u16>().unwrap());
+            
+            let pool_options = RXQLitePoolOptions::new();
+            let pool = pool_options.connect_with(connect_options).await.unwrap();
+            
+            sqlx::query(
+                "CREATE TABLE IF NOT EXISTS _sqlx_rxqlite_test_user_and_date_ (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL UNIQUE,
+            birth_date DATETIME NOT NULL
+        )",
+            )
+            .execute(&pool)
+            .await
+            .unwrap();
+            let birth_date = Utc::now();
+            let name = format!("Ha-{}",i);
+            sqlx::query(
+                "INSERT INTO _sqlx_rxqlite_test_user_and_date_ (name,birth_date) VALUES (?, ?);",
+            )
+            .bind(&name)
+            .bind(&birth_date)
+            .execute(&pool)
+            .await
+            .unwrap();
+
+            let rows = sqlx::query("SELECT * FROM _sqlx_rxqlite_test_user_and_date_ WHERE name = ?")
+                .bind(&name)
+                .fetch_all(&pool)
+                .await
+                .unwrap();
+            assert_eq!(rows.len(), 1);
+            let row = &rows[0];
+            let fetched_name: String = row.get(1);
+            assert_eq!(&fetched_name, &name);
+            let fetched_birth_date: DateTime<Utc> = row.get(2);
+            assert_eq!(fetched_birth_date, birth_date);
+          });
+        }
+        join_all(futs).await;
+    });
+}
